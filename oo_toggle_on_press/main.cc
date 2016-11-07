5bde8734c5923210f151ed5c33e98b49fe8e7651
Leland Jansen <lelandjansen@users.noreply.github.com>
2016-11-06 23:06:46 -0800

    Staring work on LED PWM

--- oo_toggle_on_press/main.cc
+++ oo_toggle_on_press/main.cc
@@ -32,15 +32,53 @@ void __cxa_pure_virtual(void) {};
 
 
 
+namespace helper {
+  template <class T> const T &Swap(const T &a, const T &b) {
+    a ^= b;
+    b ^= a;
+    a ^= b;
+  }
+
+  template <class T> const T &Confine(const T &x, const T &lower_bound, const T &upper_bound) {
+    if (upper_bound < lower_bound) Swap(lower_bound, upper_bound);
+    if (x < lower_bound) return lower_bound;
+    if (upper_bound < x) return upper_bound;
+    return x;
+  }
+
+  template <class T> const T &Scale(const T &x, const T &x_min, const T &x_max, const T &new_min, const T &new_max) {
+    return new_min + (new_max - new_min) * x / (x_max - x_min);
+  }
+}
+
+
+
+
+struct Color {
+  Color(unsigned int red, unsigned int greed, unsigned int blue);
+  unsigned int red;
+  unsigned int green;
+  unsigned int blue;
+}
+
+Color::Color(unsigned int red, unsigned int green, unsigned int blue)  : red(red), green(green), blue(blue) {}
+
+
+
 class Led {
  public:
-   static Led &instance();
-   void Setup() const;
-   void On() const;
-   void Off() const;
-   void Toggle() const;
+  static Led &instance();
+  void Setup() const;
+  void On() const;
+  void Off() const;
+  void Toggle() const;
+  void SetColor(Color color) const;
+  void Pulse(Color color1, Color color2);
  private:
-   Led();
+  Led();
+  const bool common_anode = false;
+  int cycle_ = 0;
+  const int cycle_size_ = 255;
 };
 
 Led::Led() {}
@@ -74,7 +112,19 @@ void Led::Toggle() const {
   PORTB ^= (1<<PORTB1);
 }
 
+void Led::SetColor(Color color) const {
+  if (common_anode) {
+    color.red   = UINT_MAX - color.red;
+    color.green = UINT_MAX - color.green;
+    color.blue  = UINT_MAX - color.blue;
+  }
+}
 
+void Led::Pulse(Color color1, Color color2) {
+  cycle_ = (++cycle_) % cycle_size_;
+  // scale
+  // set color
+}
 
 
 
@@ -102,7 +152,6 @@ void Button::Setup() const {
   EICRA |= (1 << ISC01); // Set INT0 to trigger on any logic change
   EIMSK |= (1 << INT0); // Turn on INT0
 }
-
 
 ISR(INT0_vect) {
   Led::instance().Toggle();
